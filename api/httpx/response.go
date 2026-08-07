b3ba4997cb0ae67574d535a71ca3219cb4dcc2dc
madokast <578562554@qq.com>
2026-08-07 08:49:02 +0000

    先jsonify后返回http

--- api/httpx/response.go
+++ api/httpx/response.go
@@ -2,6 +2,8 @@ package httpx
 
 import (
 	"encoding/json"
+	"fmt"
+	"log/slog"
 	"net/http"
 )
 
@@ -11,8 +13,16 @@ type Response interface {
 }
 
 func WriteJSON[R Response](w http.ResponseWriter, response R) {
+	bs, err := json.Marshal(response)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		msg := fmt.Sprintf(`{"ok":false,"status":500, "error":"failed to marshal response: %s"}`, err.Error())
+		_, _ = w.Write([]byte(msg))
+		return
+	}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(response.GetStatus())
-
-	_ = json.NewEncoder(w).Encode(response)
+	_, err = w.Write(bs)
+	slog.Error("failed to write response", "err", err)
 }
